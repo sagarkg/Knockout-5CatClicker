c0910d723e645c7fa70a10f71950080e1e0f3300
Sagar Ganatra <sagar.ganatra@gmail.com>
2015-05-03 17:16:56 -0400

    Working cat clicker with multiple cat data

--- js/app.js
+++ js/app.js
@@ -1,11 +1,49 @@
 
+"use strict";
 
-var Cat = function(){
+//Cat Data
+var initialCats = [
+	{
+		name: "Cattie",
+		image: "images/cat.jpg", 
+		count: 0,
+		petNames: ["Bert","Mert", "Sert","Gert"]
 
-	this.name = ko.observable("Cattie");
-	this.image = ko.observable('images/cat.jpg');
-	this.count = ko.observable(0);
-	this.petNames = ko.observableArray(["Bert","Mert", "Sert","Gert"]);
+	},
+
+	{	
+		name: "Chewie", 
+		image: "images/cat2.jpg", 
+		count: 0,
+		petNames: ["Jon","Von", "Con","Don"]
+	},
+	{	name: "Zestie", 
+		image: "images/cat3.jpg" , 
+		count: 0,
+		petNames: ["Zee","Zac", "Zest","Zen"]
+	},
+	{
+		name: "Grumpie" , 
+		image: "images/cat4.jpg", 
+		count: 0,
+		petNames: ["Grump","Pump"]
+	},
+	{
+		name: "Cutie", 
+		image: "images/cat5.jpg" , 
+		count: 0,
+		petNames: ["Cuteness","Cutiepie", "Cuttieshwar","Cuttieben"]
+	}
+	];
+
+
+//Cat Model Constructor Function 
+var Cat = function(data){
+
+	this.name = ko.observable(data.name);
+	this.image = ko.observable(data.image);
+	this.count = ko.observable(data.count);
+	this.petNames = ko.observableArray(data.petNames);
 	
 	this.catLevel = ko.computed(function(){
 
@@ -36,13 +74,31 @@ var Cat = function(){
 
 }
 
+
+//KO ViewModel
 var ViewModel = function(){
 	var self = this;
-	this.currentCat = ko.observable(new Cat());
+	self.catList = ko.observableArray([]);
+	
+	initialCats.forEach(function(catItem){
+		self.catList.push(new Cat(catItem));
+	});
+
+
+	self.currentCat = ko.observable(self.catList()[0]);
 
 	self.incrementCounter = function(){
 		self.currentCat().count(self.currentCat().count() + 1);
 	};
+
+	self.setCurrentCat = function(obj, event){
+		
+		self.currentCat(obj);
+		
+	};
+
+	
+
 }
 
 ko.applyBindings(new ViewModel());
