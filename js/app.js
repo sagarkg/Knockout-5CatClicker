18bf652003cc706c31e2ed32f7b5715e6fcb6f3d
Sagar Ganatra <sagar.ganatra@gmail.com>
2015-05-03 14:39:04 -0400

    Separated Model from ViewModel
    Separated Model from ViewModel. Used with binding to simplify html
    binding.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 
-var ViewModel = function(){
+
+var Cat = function(){
+
 	this.name = ko.observable("Cattie");
 	this.image = ko.observable('images/cat.jpg');
 	this.count = ko.observable(0);
 	this.petNames = ko.observableArray(["Bert","Mert", "Sert","Gert"]);
 	
 	this.catLevel = ko.computed(function(){
+
 		if(this.count() <= 10){
 			return "New Born";
 		}
@@ -31,8 +34,14 @@ var ViewModel = function(){
 
 	}, this);
 
-	this.incrementCounter = function(){
-		this.count(this.count() + 1);
+}
+
+var ViewModel = function(){
+	var self = this;
+	this.currentCat = ko.observable(new Cat());
+
+	self.incrementCounter = function(){
+		self.currentCat().count(self.currentCat().count() + 1);
 	};
 }
 
