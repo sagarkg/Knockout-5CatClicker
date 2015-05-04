b821b7975c266f4c8de3232a5d00a614cfacd5fa
Sagar Ganatra <sagar.ganatra@gmail.com>
2015-05-04 18:50:05 -0400

    Working Admin Panel
    Added data binding for admin panel fields. Next --> to add change Admin
    Panel data when a new cat is loaded from the cat list

--- js/app.js
+++ js/app.js
@@ -39,11 +39,12 @@ var initialCats = [
 
 //Cat Model Constructor Function 
 var Cat = function(data){
-
+	
 	this.name = ko.observable(data.name);
 	this.image = ko.observable(data.image);
 	this.count = ko.observable(data.count);
 	this.petNames = ko.observableArray(data.petNames);
+	
 	
 	this.catLevel = ko.computed(function(){
 
@@ -87,8 +88,17 @@ var ViewModel = function(){
 
 	self.currentCat = ko.observable(self.catList()[0]);
 
+	//Admin Panel variables
+	self.tmpName= ko.observable(self.currentCat().name());
+	self.tmpImage = ko.observable(self.currentCat().image());
+	self.tmpCount = ko.observable(self.currentCat().count());
+
+	//AdminView Toggling variable
+	self.adminVisibleFlag = ko.observable(false);
+
 	self.incrementCounter = function(){
 		self.currentCat().count(self.currentCat().count() + 1);
+		self.tmpCount(self.currentCat().count());
 	};
 
 	self.setCurrentCat = function(obj, event){
@@ -97,14 +107,25 @@ var ViewModel = function(){
 		
 	};
 
-	//AdminView Toggling
-	self.adminVisibleFlag = ko.observable(false);
-
+	
 	self.toggleAdminView = function(){
 		self.adminVisibleFlag(!self.adminVisibleFlag());
-		
+		self.tmpName(self.currentCat().name());
+		self.tmpImage(self.currentCat().image());
+		self.tmpCount(self.currentCat().count());
+
 	};
 
+		
+	self.applyCatUpdates = function(obj,event){
+		self.currentCat().name(self.tmpName());
+		self.currentCat().image(self.tmpImage());
+		if(!isNaN(parseInt(self.tmpCount()))){
+			self.currentCat().count(parseInt(self.tmpCount()));
+		}else {
+		 throw alert("Count has to be a number");
+		}
+	};	
 };
 
 ko.applyBindings(new ViewModel());
